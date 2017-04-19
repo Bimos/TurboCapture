9159db5dc3a7c8d77d65c1f00202f603e3e63fce
unknown <>
2017-04-19 12:48:50 +0800

    test

--- clipper.py
+++ clipper.py
@@ -2,9 +2,25 @@ import cv2
 import numpy as np
 from PIL import ImageGrab
 import os
+import csv
+base_path = 'data'
 
 
-def matching(imgname, templatename, resultname):
+
+def src_(s):
+    return s+'src.png'
+
+def res_(s):
+    return s+'res.png'
+
+def tmp_(s):
+    return s+'tmp.png'
+
+def matching(s):
+    templatename = tmp_(s)
+    imgname = src_(s)
+    resultname = res_(s)
+
     img = cv2.imread(imgname, 0)
     template = cv2.imread(templatename, 0)
 
@@ -12,38 +28,48 @@ def matching(imgname, templatename, resultname):
         return (int(h * w_h_ratio), h)
 
     (hm, wm) = img.shape
-    print wm,hm
-
     (h,w) = template.shape
     w_h_ratio = 1. * w / h
 
-    height_min = hm / 10
-    step = hm / 30
-
+    height_min = hm / 5
+    step = hm / 40
+    # step = 1
     maxVal = None
     for h in xrange(height_min, hm, step):
         w,h = size(h)
 
         if w > wm:
             break
-
-        print w,h
         resized = cv2.resize(template,(w,h),
          interpolation=cv2.INTER_AREA)
         res = cv2.matchTemplate(img, resized,cv2.TM_CCOEFF_NORMED)
         _,t,_,loc = cv2.minMaxLoc(res)
-
+        # t,_,loc,_ = cv2.minMaxLoc(res)
+        # if (maxVal is None or t < maxVal):
         if (maxVal is None or t > maxVal):
             maxVal = t
             top_left = loc
             bottom_right = (loc[0]+w, loc[1]+h)
+            hmaaa = h
 
 
+    print "similar = ", maxVal
+    print "hmaaa = ", hmaaa
     colorful = cv2.imread(imgname)
     result = colorful[top_left[1]:bottom_right[1],
                       top_left[0]:bottom_right[0]]
     cv2.imwrite(resultname, result)
 
+    print top_left, bottom_right, hm, wm
+    with open(s+'res.txt','w') as f:
+        __little_helper(f,
+            [1.*top_left[0]/wm,1.*top_left[1]/hm,1.*bottom_right[0]/wm,1.*bottom_right[1]/hm])
+
+def __little_helper(f,lst):
+    for n in lst:
+        f.write(str(n))
+        f.write('\n')
+    f.flush()
 
 def getCapture(sourcename):
     p = ImageGrab.grab()
@@ -53,15 +79,11 @@ def fromTemplate(tmp):
     getCapture(src(tmp))
     matching(src(tmp), tmp, res(tmp))
 
-def src(s):
-    return "src_" + s
-
-def res(s):
-    return "res_" + s
 
 
 
 def clean(s):
     os.remove(s)
     os.remove(src(s))
-    os.remove(res(s))
+    os.remove(res_(s))
+    pass
